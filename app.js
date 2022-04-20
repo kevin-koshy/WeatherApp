86e4a1a45a29bc713df7559785fd465d1163e262
Kevin Koshy <kevin999koshy@gmail.com>
2022-04-20 11:18:56 +0530

    Final Commit

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.listen(3000, function() {
     console.log("Sever started on port 3000.");
 })
 
 app.get("/", function(req, res){
-    const query = "London";
+    res.sendFile(__dirname+ "/index.html");
+
+})
+
+app.post("/", function (req, res){
+    console.log(req.body.cityName);
+
+    const query = req.body.cityName;
     const apiKey = "390e3e00a059a4ec5c4b3c5708ba1189";
     const unit = "metric";
     const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&units="+unit+"&appid="+apiKey;
@@ -21,10 +31,11 @@ app.get("/", function(req, res){
             const icon =weatherData.weather[0].icon;
             const imageURL =  "http://openweathermap.org/img/wn/"+icon+"@2x.png";
             res.write("<p>The weather is currently "+weatherDesc+"</p>");
-            res.write("<p><h1>The temperature is currently "+temp+" degree Celsius</h1></p>");
+            res.write("<p><h1>The temperature in "+query+" is currently "+temp+" degree Celsius</h1></p>");
             res.write("<img src = "+imageURL+">");
             res.send();
 
         })
     });
 })
+
